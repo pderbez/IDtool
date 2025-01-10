baa6ef43ec0ed2c5922f8d0eee09f4bdb127a4e0
marie.euler <marie.euler@irisa.fr>
2025-01-10 13:10:08 +0100

    Attaque Simon à affiner

---
+++ SIMON/SIMON_Generator_Attack.py
@@ -0,0 +1,330 @@
+import gurobipy as gp
+import SIMON_Validator 
+from draw_SIMON import draw
+
+
+def getSummaryBit(M, a, b):
+    # 0+0 =0 , *+0 = 0 , *+* = *
+    # Non 0,0,*
+    # Non 0,_ 1
+    s = M.addVar(vtype="b")
+    M.addConstr(2*s <= a+b)
+
+    M.addConstr(a+b <= 1+s)
+    return s
+
+
+def getSummary(M, X, Y):
+    n = len(X[0])
+    return [[getSummaryBit(M, X[j][i], Y[j][i]) for i in range(n)] for j in range(3)]
+
+
+def AND(M, a, b, c=None):
+    # 0 x 0 = 0
+    # * x * = *
+    # * x 0 = *
+    if c is None:
+        c = M.addVar(vtype="b")
+    M.addConstr(a+b >= c)
+    M.addConstr(2*c >= a+b)
+ 
+    return c
+
+
+def XOR(M, a, b, s=None):
+    if s is None:
+        s = M.addVar(vtype="b")
+
+    M.addConstr(a +b <= 2*s)
+    M.addConstr(a+b >= s)
+
+    return s
+
+def XORList(M, L, s=None): 
+    if s is None:
+        s = M.addVar(vtype="b")
+
+    M.addConstr(sum(L) <= len(L)*s)
+    M.addConstr(sum(L) >= s)
+
+    return s
+
+
+def XORState(M, X, Y, Z=None):
+    if Z is None:
+        Z = [M.addVar(vtype="b") for _ in range(len(X))]
+    for i in range(len(X)):
+        XOR(M, X[i], Y[i], Z[i])
+    return Z
+
+def XORStateList(M, X, Z=None):
+    
+    if Z is None:
+        Z = [M.addVar(vtype="b") for _ in range(len(X[0]))] 
+    
+    for i in range(len(X[0])):
+        XORList(M, [a[i] for a in X] ,  Z[i])
+    return Z
+
+
+def ANDState(M, X, Y, Z=None):
+    if Z is None:
+        Z = [M.addVar(vtype="b") for _ in range(len(X))]
+    for i in range(len(X)):
+        AND(M, X[i], Y[i], Z[i])
+    return Z
+
+def Cmp(M,x,y,z) :
+    # if z=1 then t=1
+    #else 
+      #  t=y
+
+    t= M.addVar(vtype="b")
+    M.addConstr(1-z+t>=1)
+    M.addConstr(z+1-y+t>=1)
+    M.addConstr(z+y+1-t>=1)
+    return t
+
+
+def CmpState(M,X,Y,Z): 
+    return [Cmp(M,X[i],Y[i],Z[i]) for i in range(len(X))]
+
+
+def Rotate(state,r) :
+    return state[r:]+state[:r]
+
+def FeistelFunction(M, state):
+    return XORState(M, Rotate(state,2), ANDState(M, Rotate(state,8), Rotate(state,1)))
+
+
+
+
+
+def FindNewZerosXOR(M, a, b, c):  # c=a XOR b
+    NewZero = M.addVar(vtype="b")
+    # b = 0 => No new Zero
+    M.addConstr(b >= NewZero)
+    # b= 1  and a=c=0 => New Zero
+    M.addConstr(a+c + NewZero >= b)
+    # b=1 and a=1 or c==1 => No NewZero
+    M.addConstr(1 >= a+NewZero)
+    M.addConstr(1 >= c+NewZero)
+
+    return NewZero
+
+
+def FindNewZeros(M,R, tmp, LL):
+    n = len(R)
+    return [FindNewZerosXOR(M, R[i],tmp[i],LL[i]) for i in range(n)]
+
+
+def my_callback(model, where):
+
+    if where == gp.GRB.Callback.MIPSOL:
+
+        model._solCount += 1
+        n = len(model._summary[0][0])
+
+        target_i = [[round(model.cbGetSolution(model._summary[0][0][i]))
+                    for i in range(n)], [round(model.cbGetSolution(model._summary[0][1][i])) for i in range(n)]]
+        target_o = [[round(model.cbGetSolution(model._summary[-1][0][i]))
+                    for i in range(n)], [round(model.cbGetSolution(model._summary[-1][1][i])) for i in range(n)]]
+
+        rD = len(model._summary)
+
+    
+        s = SIMON_Validator.is_differential_possible(
+            target_i, target_o, rD-1, n)
+     
+        if s == gp.GRB.INFEASIBLE:
+            nb_one = sum(target_i[0])+sum(target_i[1])+sum(target_o[0])+sum(target_o[1])
+            print(f"Found an impossible differential provoked by {
+                  round(sum(model.cbGetSolution(model._NewZeros)))} zeros. It has {nb_one} active bits")
+            print("ID:", "".join([str(x) for x in target_i[0]]) + " "+"".join([str(x) for x in target_i[1]]),
+                  " -/->  "+"".join([str(x) for x in target_o[0]]) + " " + "".join([str(x) for x in target_o[1]]))
+
+            draw(model, "SIMON")
+
+            model._valid += 1
+            print(model.cbGet(gp.GRB.Callback.RUNTIME) )
+
+        else:
+            if model._solCount %50 ==0 :
+                print(f"solution {model._solCount} is possible .. we exclude it")
+
+            c = 0
+            for i in range(len(target_i[0])):
+                if round(target_i[0][i]) == 1:
+                    c += 1-model._summary[0][0][i]
+                # else:
+                #     c += model._summary[0][0][i]
+                if round(target_o[0][i]) == 1:
+                    c += 1-model._summary[-1][0][i]
+                # else:
+                #     c += model._summary[-1][0][i]
+                if round(target_i[1][i]) == 1:
+                    c += 1-model._summary[0][1][i]
+                # else:
+                #     c += model._summary[0][1][i]
+                if round(target_o[1][i]) == 1:
+                    c += 1-model._summary[-1][1][i]
+                # else:
+                #     c += model._summary[-1][1][i]
+
+            model.cbLazy(c >= 1)
+
+def newState(M,n) :
+    return [M.addVar(vtype='b') for _ in range(n)]
+
+
+def find_impossible_differential(rB,rD,rF, n):
+    M = gp.Model()
+    M.setParam("LazyConstraints", 1)
+
+    M._is_zero_forward = []
+    M._is_zero_backward = []
+
+    M._NewZeros = []
+
+    M._solCount = 0
+    M._valid = 0
+
+    Lf = newState(M,n)
+    Lb = newState(M,n)
+    M._is_zero_forward = [[Lf, newState(M,n), FeistelFunction(M, Lf)] ] +  [[newState(M,n), None ,None ] for  _ in range(rB+rD)]
+    M._is_zero_backward = [[Lb, newState(M,n), FeistelFunction(M,Lb)]] +  [[newState(M,n), None ,None ] for  _ in range(rF+rD)]
+
+    M._value_needed_backward = [[newState(M,n), newState(M,n), None]] +  [[newState(M,n), None ,None ] for  _ in range(rB)]
+    M._value_needed_forward =  [[newState(M,n), newState(M,n), None]] +  [[ None, newState(M,n),None ] for  _ in range(rF)]
+
+    M._key_recovery_forward = []
+    M._key_recovery_backward = []
+
+    # Key recovery (backward propagation)
+    for r in range(rB):
+        XORState(M, M._is_zero_forward[r+1][0],
+                 M._is_zero_forward[r][2], M._is_zero_forward[r][1])
+        
+        M._is_zero_forward[r+1][1] =  M._is_zero_forward[r][0]
+        M._is_zero_forward[r+1][2] = FeistelFunction(M, M._is_zero_forward[r+1][0])
+
+        M._value_needed_backward[r][2] = M._value_needed_backward[r+1][0]
+        M._value_needed_backward[r][1] = M._value_needed_backward[r+1][0]
+
+    M._value_needed_backward[rB][1] = newState(M,n) 
+
+    for r in range(rB) : 
+        ValueNeededInX = CmpState(M,Rotate(M._is_zero_forward[r][0],8), Rotate(M._is_zero_forward[r][0],1), M._value_needed_backward[r][2]  )
+        ValueNeededInY = CmpState(M,Rotate(M._is_zero_forward[r][0],1), Rotate(M._is_zero_forward[r][0],8),  M._value_needed_backward[r][2] )
+        M._key_recovery_forward.append(XORState(M, Rotate(ValueNeededInX,-8), Rotate(ValueNeededInY,-1)))
+   
+        XORStateList(M, [M._value_needed_backward[r+1][1] , Rotate(M._value_needed_backward[r][2],-2),  M._key_recovery_forward[-1]] , M._value_needed_backward[r][0])
+       
+
+    M.addConstrs(M._value_needed_backward[rB][0][i] == 0 for i in range(n))
+    M.addConstrs(M._value_needed_backward[rB][1][i] == 0 for i in range(n))
+
+    # M.addConstrs(M._is_zero_forward[rB][0][i] == 0 for i in range(n))
+    # for i in range(n) :
+    #     if i in  [6] : #[1,3,10] :
+    #         M.addConstr(M._is_zero_forward[rB][1][i]==1)
+    #     else :
+    #         M.addConstr(M._is_zero_forward[rB][1][i]==0)
+
+
+    # Distinguisher
+    for r in range(rD):
+
+        XORState(M, M._is_zero_forward[rB+r][1], M._is_zero_forward[rB+r][2], M._is_zero_forward[rB+r+1][0])
+
+        XORState(M, M._is_zero_backward[r+1][0],
+                 M._is_zero_backward[r][2], M._is_zero_backward[r][1])
+
+        
+        M._is_zero_forward[rB+r+1][1] =  M._is_zero_forward[rB+r][0]
+        M._is_zero_backward[r+1][1] =  M._is_zero_backward[r][0]
+
+        M._is_zero_forward[rB+r+1][2] =  FeistelFunction(M,M._is_zero_forward[rB+r+1][0])
+        M._is_zero_backward[r+1][2] = FeistelFunction(M, M._is_zero_backward[r+1][0])
+
+
+
+    M._summary= [getSummary(M, M._is_zero_forward[rB+r], M._is_zero_backward[r]) for r in range(rD+1)]
+    
+    for r in range(rD):
+        M._NewZeros += FindNewZeros(M, M._summary[r][1], M._summary[r][2], M._summary[r+1][0])
+
+    M.addConstr(gp.quicksum(M._NewZeros)>=1)
+
+    # Key recovery (forward propagation )
+    for r in range(rD, rD+rF):
+        XORState(M, M._is_zero_backward[r][1],
+                 M._is_zero_backward[r][2], M._is_zero_backward[r+1][0])
+        M._is_zero_backward[r+1][1] =  M._is_zero_backward[r][0]
+        M._is_zero_backward[r+1][2] = FeistelFunction(M, M._is_zero_backward[r+1][0])
+
+    for r in range(rF) :
+        M._value_needed_forward[r][2] = M._value_needed_forward[r][1]
+        M._value_needed_forward[r+1][0] = M._value_needed_forward[r][1]
+
+    M._value_needed_forward[-1][1] = newState(M,n) 
+
+    for r in range(rF) : 
+        ValueNeededInX = CmpState(M,Rotate(M._is_zero_backward[rD+r+1][1],8), Rotate(M._is_zero_backward[rD+r+1][1],1), M._value_needed_forward[r][2]  )
+        ValueNeededInY = CmpState(M,Rotate(M._is_zero_backward[rD+r+1][1],1), Rotate(M._is_zero_backward[rD+r+1][1],8),  M._value_needed_forward[r][2] )
+        M._key_recovery_backward.append(XORState(M, Rotate(ValueNeededInX,-8), Rotate(ValueNeededInY,-1)))
+
+        XORStateList(M, [M._value_needed_forward[r][0] , Rotate(M._value_needed_forward[r][2],-2),  M._key_recovery_backward[-1]] , M._value_needed_forward[r+1][1])
+       
+
+
+    M.addConstrs(M._value_needed_forward[0][0][i] == 0 for i in range(n))
+    M.addConstrs(M._value_needed_forward[0][1][i] == 0 for i in range(n))
+
+
+    # M.addConstrs(M._summary[-1][1][i] == 0 for i in range(n))
+    # for i in range(n) :
+    #     if i in [1] :
+    #         M.addConstr(M._summary[-1][0][i]==1)
+    #     else :
+    #         M.addConstr(M._summary[-1][0][i]==0)
+
+   
+    # Mode 1 : maximize number of zeros 
+    
+    # M.setObjective(-gp.quicksum(M._NewZeros))
+
+    # Mode 2 : Maximize number of active bits  (with heuristics Number of zeros >=3)
+    M.addConstr(gp.quicksum(M._NewZeros) >= 3)
+    # M.setObjective(-(gp.quicksum(M._is_zero_forward[0][0]) + gp.quicksum(M._is_zero_forward[0][1]) +
+                #    gp.quicksum(M._is_zero_backward[-1][0]) +  gp.quicksum(M._is_zero_backward[-1][1])))
+
+
+    # M.addConstr(M._summary[0][0][0] + M._summary[0][1][0]>=1)
+
+    M.setObjective(gp.quicksum(gp.quicksum(x) for x in M._key_recovery_forward[1:])+ gp.quicksum(gp.quicksum(x) for x in M._key_recovery_backward[:rF-1]))
+
+    M.optimize(my_callback)
+    print(M.objVal)
+
+    print(f"============= {rD} rounds == SIMON-{2*n} ==  Explored {
+          M._solCount} solutions and found {M._valid} ID in { round(M.Runtime,2)} seconds ==========")
+
+
+# find_impossible_differential(3,11,5, 16)
+find_impossible_differential(4,11,5,16)
+
+# find_impossible_differential(12, 24)
+
+# find_impossible_differential(5,13,5, 32)
+
+
+# find_impossible_differential(16, 48)
+
+# find_impossible_differential(4,19,4, 64)
+
+# find_impossible_differential(12, 16)
+# find_impossible_differential(13, 24)
+# find_impossible_differential(14, 32)
+# find_impossible_differential(17, 48)
+# find_impossible_differential(20, 64)
--- SIMON/draw_SIMON.py
+++ SIMON/draw_SIMON.py
@@ -1,6 +1,9 @@
 COLOR_FORWARD_DISTINGUISHER = "WildStrawberry!80"
 COLOR_BACKWARD_DISTINGUISHER = "NavyBlue!80"
 COLOR_COL_NEW_ZEROS = "ForestGreen!90"
+COLOR_INVOLVED_CELLS_F = "yellow!50"
+COLOR_INVOLVED_CELLS_B = "green!50"
+COLOR_VALUE_NEEDED_F = "blue!60"
 
 
 
@@ -20,6 +23,15 @@ def drawGrid(x,y, file_name, UpperTrail, LowerTrail, NewZeros) :
                 if NewZeros[j]!=0 :
                     f.write(f"\\draw[draw={COLOR_COL_NEW_ZEROS}, ultra thick]({x+j*w},{y-0.1}) rectangle  ++ ({w},1.2); \n")
 
+def NeededGrid(x,y, file_name, neededTrail) :
+
+    w = min(len(neededTrail)//2, 32) *1.0/ len(neededTrail)
+    with open(file_name, "a") as f:
+        for j in range(len(neededTrail)):
+            if neededTrail[j] == 1:
+                f.write(f"\\draw[draw={COLOR_VALUE_NEEDED_F},ultra thick] ({x+j*w},{y+1} )--++({w},-1);\n")
+            f.write(f"\\draw ({x+j*w},{y}) rectangle  ++ ({w},1); \n")
+
 def drawFeistel(y, w,file_name ) :
     with open(file_name, "a") as f:
         f.write(f"\\draw[->]({w//2},{y}) -- ++(0,-1) -- ++(5,0) node[midway, draw, fill =white] "+"{ S } ;\n")
@@ -34,15 +46,28 @@ def drawFeistel(y, w,file_name ) :
 
 def draw(model, algo):
 
-    rD = len(model._is_zero_forward)-1
-    n = len(model._is_zero_forward[0][0])
+    rD = len(model._summary)-1
+
+    rB = len(model._is_zero_forward)-len(model._summary)
+    rF = len(model._is_zero_backward)-len(model._summary)
+    n = len(model._summary[0][0])
     w = min(n//2, 32)
 
     file_name = f"{algo}_{rD}_{n}_{model._valid}.tex"
 
+  
+    Backward_rounds=  [[[round(model.cbGetSolution(model._is_zero_forward[r][a][j]))  for j in range(n)]  for a in range(3)] for r in range(rB)]
+    Forward_rounds =  [[[round(model.cbGetSolution(model._is_zero_backward[r][a][j]))  for j in range(n)]  for a in range(3)] for r in range(rD,rD+rF+1)]
 
-    UpperTrail = [[[round(model.cbGetSolution(model._is_zero_forward[r][a][j]))  for j in range(n)]  for a in range(3)] for r in range(rD+1)]
+    UpperTrail = [[[round(model.cbGetSolution(model._is_zero_forward[rB+r][a][j]))  for j in range(n)]  for a in range(3)] for r in range(rD+1)]
     LowerTrail = [[[round(model.cbGetSolution(model._is_zero_backward[r][a][j])) for j in range(n)]  for a in range(3)] for r in range(rD+1)]
+
+    Backward_NeededKR = [[[round(model.cbGetSolution(model._value_needed_backward[r][a][j]))  for j in range(n)]  for a in range(3)] for r in range(rB)]
+    KR = [[round(model.cbGetSolution(model._key_recovery_forward[r][j]))  for j in range(n)]  for r in range(rB)]
+
+    Forward_NeededKR = [[[round(model.cbGetSolution(model._value_needed_forward[r][a][j]))  for j in range(n)]  for a in range(3)] for r in range(rF)]
+    BKR = [[round(model.cbGetSolution(model._key_recovery_backward[r][j]))  for j in range(n)]  for r in range(rF)]
+    
 
     NewZeros = [round(model.cbGetSolution(x)) for x in model._NewZeros]
    
@@ -53,13 +78,48 @@ def draw(model, algo):
         f.write("\\begin{document}\n")
         f.write("\\begin{tikzpicture}\n")
 
-    for r in range(len(UpperTrail)) :
 
-        drawGrid(0,- 4*r, file_name, UpperTrail[r][0], LowerTrail[r][0],  NewZeros[n*(r-1):n*(r)] if r>0  else None)
-        drawGrid(w+10, - 4*r, file_name, UpperTrail[r][1], LowerTrail[r][1],  NewZeros[n*r:n*(r+1)] if r+1<len(UpperTrail)  else None )
+    for r in range(len(Backward_rounds)) :
+        z= 4*len(Backward_rounds) - 4*r
+        drawGrid(0, z, file_name,Backward_rounds[r][0], [0]*n,   None)
+        drawGrid(w+10, z,file_name, Backward_rounds[r][1], [0]*n, None )
+        drawGrid(w/2 + 5 ,z-1.5,file_name, Backward_rounds[r][2], [0]*n, None)      
+
+        NeededGrid(0,z,file_name, Backward_NeededKR[r][0])
+        NeededGrid(w+10,z,file_name, Backward_NeededKR[r][1])
+        NeededGrid(w/2+5,z-1.5, file_name, Backward_NeededKR[r][2])
+
+        NeededGrid(-16, z, file_name,KR[r] )
+        drawFeistel(z, w , file_name)
+
+
+    for r in range(len(UpperTrail)) :
+        z = -4*r
+        drawGrid(0,z, file_name, UpperTrail[r][0], LowerTrail[r][0],  NewZeros[n*(r-1):n*(r)] if r>0  else None)
+        drawGrid(w+10, z, file_name, UpperTrail[r][1], LowerTrail[r][1],  NewZeros[n*r:n*(r+1)] if r+1<len(UpperTrail)  else None )
         if r+1<len(UpperTrail) :
-            drawGrid(w/2 + 5 , -4*r-1.5,file_name, UpperTrail[r][2], LowerTrail[r][2], NewZeros[n*r:n*(r+1)])      
-            drawFeistel(-4*r, w , file_name)
+            drawGrid(w/2 + 5 , z-1.5,file_name, UpperTrail[r][2], LowerTrail[r][2], NewZeros[n*r:n*(r+1)])      
+            drawFeistel(z, w , file_name)
+
+    
+    for r in range(len(Forward_rounds)) :
+        z = -4*(len(UpperTrail)-1)-4*r
+        drawGrid(0, z, file_name,[0]*n, Forward_rounds[r][0],    None)
+        drawGrid(w+10, z, file_name, [0]*n, Forward_rounds[r][1], None )
+        if r+1<len(Forward_rounds) :
+            drawGrid(w/2 + 5 ,z-1.5,file_name, [0]*n, Forward_rounds[r][2],  None)      
+            drawFeistel(z, w , file_name)
+            NeededGrid(-16, z, file_name,BKR[r] )
+
+            
+            NeededGrid(0,z,file_name, Forward_NeededKR[r][0])
+            NeededGrid(w+10,z,file_name, Forward_NeededKR[r][1])
+            NeededGrid(w/2+5,z-1.5, file_name, Forward_NeededKR[r][2])
+
+            
+        
+
+
 
     with open(file_name, "a") as f:
         f.write('\\end{tikzpicture}\n\\end{document}\n')
